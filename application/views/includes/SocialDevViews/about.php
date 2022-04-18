81ba7250aafafcdeb8617b7ffc027191516e360d
Baki Baran Özel <bbaranozel0@gmail.com>
2022-04-19 01:13:14 +0300

    about

--- application/views/includes/SocialDevViews/about.php
+++ application/views/includes/SocialDevViews/about.php
@@ -2,17 +2,31 @@
         <section class="page-section bg-masthead text-white mb-0" id="about">
             <div class="container">
                 <!-- About Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+                <h2 class="page-section-heading text-center text-uppercase text-white">Hakkımızda</h2>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
                     <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                     <div class="divider-custom-line"></div>
                 </div>
+                <div class="col-lg-5 mx-auto"><p class="lead text-center">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+
+                
                 <!-- About Section Content-->
+
+                <div class="row pt-5">
+                    <div class="col-lg-4 ms-auto text-center text-white"><h2 class="page-section-heading">Volkan Ket</h2></div>
+                    <div class="col-lg-4 ms-auto text-center text-white"><h2 class="page-section-heading">Baran Özel</h2></div>
+                    <div class="col-lg-4 ms-auto text-center text-white"><h2 class="page-section-heading">Can Seven</h2></div>
+                </div>
+
+                
+
                 <div class="row">
                     <div class="col-lg-4 ms-auto"><p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
                     <div class="col-lg-4 me-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+                    <div class="col-lg-4 me-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+
                 </div>
                 <!-- About Section Button-->
                 <div class="text-center mt-4">
