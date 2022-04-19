ef63f4673d8bfd009cb7793dd68e9f4cccc32572
hanificanseven <hanifi.anelka@hotmail.com>
2022-04-19 23:54:49 +0300

    bi kaç degisiklik

--- application/views/includes/SocialDevViews/navbar.php
+++ application/views/includes/SocialDevViews/navbar.php
@@ -5,13 +5,13 @@
         <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button"
             data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
             aria-expanded="false" aria-label="Toggle navigation">
-            Menu
+            Menü
             <i class="fas fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
-                        href="#portfolio">Portfolio</a></li>
+                        href="#portfolio">Postlar</a></li>
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
                         href="#about">Hakkımızda</a></li>
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
