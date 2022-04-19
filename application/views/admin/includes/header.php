43b1100c06d3cbfe186e7775e410fd9e5c2ffbc8
hanificanseven <hanifi.anelka@hotmail.com>
2022-04-19 16:42:42 +0300

    admin panel favicon

--- application/views/admin/includes/header.php
+++ application/views/admin/includes/header.php
@@ -4,25 +4,30 @@
     <title>Social Dev Hub</title>
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="<?php echo base_url("assets/adminAssets/fontawesome-free/css/all.min.css") ?>">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="<?php echo base_url("assets/adminAssets/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css") ?>">
+    <link rel="stylesheet"
+        href="<?php echo base_url("assets/adminAssets/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css") ?>">
     <!-- iCheck -->
-    <link rel="stylesheet" href="<?php echo base_url("assets/adminAssets/icheck-bootstrap/icheck-bootstrap.min.css") ?>">
+    <link rel="stylesheet"
+        href="<?php echo base_url("assets/adminAssets/icheck-bootstrap/icheck-bootstrap.min.css") ?>">
     <!-- JQVMap -->
     <link rel="stylesheet" href="<?php echo base_url("assets/adminAssets/jqvmap/jqvmap.min.css") ?>">
     <!-- Theme style -->
     <link rel="stylesheet" href="<?php echo base_url("assets/adminAssets/dist/css/adminlte.min.css") ?>">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="<?php echo base_url("assets/adminAssets/overlayScrollbars/css/OverlayScrollbars.min.css") ?>">
+    <link rel="stylesheet"
+        href="<?php echo base_url("assets/adminAssets/overlayScrollbars/css/OverlayScrollbars.min.css") ?>">
     <!-- Daterange picker -->
     <link rel="stylesheet" href="<?php echo base_url("assets/adminAssets/daterangepicker/daterangepicker.css") ?>">
     <!-- summernote -->
     <link rel="stylesheet" href="<?php echo base_url("assets/adminAssets/summernote/summernote-bs4.min.css") ?>">
+    <link rel="icon" href="<?=base_url("assets");?>/img/Socialdevhublogo3.png" type="image/gif">
     <script src='<?php echo base_url("assets/adminAssets/") ?>tinymce/js/tinymce/tinymce.min.js'></script>
 
 </head>
@@ -32,5 +37,6 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="<?php echo base_url("assets/img/Socialdevhublogo.png") ?>" alt="socialdevhub" height="200" width="200">
+            <img class="animation__shake" src="<?php echo base_url("assets/img/Socialdevhublogo.png") ?>"
+                alt="socialdevhub" height="200" width="200">
         </div>
