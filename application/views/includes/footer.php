e5af8694305a7ce3a65588c757b9b411cd1d4865
hanificanseven <hanifi.anelka@hotmail.com>
2022-04-21 19:37:33 +0300

    3ünüde kabul edin lan

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -24,10 +24,10 @@
                     <!-- Footer About Text-->
                     <div class="col-lg-4 mb-5 mb-lg-0">
                         <h4 class="text-uppercase mb-4">Tasarımcılar</h4>
-                        <a class="btn btn-outline-light btn-social mx-1" target="blank" target="blank"
+                        <a class="btn btn-outline-light btn-social mx-1" target="blank"
                             href="https://www.instagram.com/canchavo7/?hl=tr">
                             <i class="fa-solid fa-user"></i> <a class="btn btn-outline-light btn-social mx-1"
-                                href="https://www.instagram.com/ketvlkn/?hl=tr">
+                                target="blank" href="https://www.instagram.com/ketvlkn/?hl=tr">
                                 <i class="fa-solid fa-user"></i> <a class="btn btn-outline-light btn-social mx-1"
                                     target="blank" href="https://www.instagram.com/bbaranozel/?hl=tr">
                                     <i class="fa-solid fa-user"></i></a>
